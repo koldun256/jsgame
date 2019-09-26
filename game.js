f7fae8c54cc71195b29dec17db30ee949d2894d6
russianKrabik <eg.sharypov@ya.ru>
2019-09-27 00:54:52 +0700

    добавлено подключение в партию

--- game.js
+++ game.js
@@ -1,48 +1,85 @@
-module.exports = function(){
-    let height = 20;
-    let width = 20;
-    let users = {};
-    function User(){
-        this.position = [0,0];
-        this.move = function(direction){
-            switch(direction){
-                case "ArrowLeft":
-                    if(((!this.position[0]-1)<0)){
-                        this.position[0]--;
-                    }
-                    break;
-                case "ArrowUp":
-                    if(!((this.position[1]-1)<0)){
-                            this.position[1]--;
-                    }
-                    break;
-                case "ArrowRight":
-                    if(!((this.position[0]+1)>(width-1))){
-                        this.position[0]++;
-                    }
-                    break;
-                case "ArrowDown":
-                    if(!((this.position[1]+1)>(height-1))){
-                        this.position[1]++;
-                    }
-                    break;
-            }
+let height = 20;
+let width = 20;
+let users = {};
+let waitingGame = null;
+function User(send){
+    this.position = [0,0];
+    this.game = null;
+    this.send = send;
+    this.toSendingData = function(){
+        return {position: this.position};
+    };
+    this.move = function(direction){
+        switch(direction){
+            case "ArrowLeft":
+                if(((!this.position[0]-1)<0)){
+                    this.position[0]--;
+                }
+                break;
+            case "ArrowUp":
+                if(!((this.position[1]-1)<0)){
+                    this.position[1]--;
+                }
+                break;
+            case "ArrowRight":
+                if(!((this.position[0]+1)>(width-1))){
+                    this.position[0]++;
+                }
+                break;
+            case "ArrowDown":
+                if(!((this.position[1]+1)>(height-1))){
+                    this.position[1]++;
+                }
+                break;
         }
     }
-    return {
-        addUser: function(id){
-            console.log("user "+id+" added");
-            users[id] = new User();
-        },
-        move: function(id,direction){
-            console.log(id+" moved");
-            users[id].move(direction);
-        },
-        getUsers: function(){
-            //console.log(users);
-            return users;
-        },
-        height: height,
-        width: width
-    };
+}
+
+module.exports = {
+    newGame: function(player, startCallback){
+        console.log("game created");
+        waitingGame = {
+            player1: player,
+            start: startCallback,
+            addPlayer: player=>{
+                waitingGame.player2 = player;
+                console.log("a");
+                startCallback();
+            }
+        };
+        player.game = waitingGame;
+    },
+    addUserToGame: function(player,startCallback2){
+        if(waitingGame == null){
+            player.send("wait");
+            this.newGame(player,()=>{
+                console.log("game started");
+                startCallback2(waitingGame.player1,waitingGame.player2);
+                waitingGame.player1.send("start",{a:"a"});
+                waitingGame.player2.game = waitingGame;
+                console.log(waitingGame.player2);
+                waitingGame.player2.send("start",{a:"a"});
+            });
+        }else{
+            waitingGame.addPlayer(player);
+
+            
+            waitingGame = null;
+        }
+    },
+    addUser: function(id,send){
+        console.log("user "+id+" added");
+        let user = new User(send);
+        users[id] = user;
+        return user;
+    },
+    move: function(id,direction){
+        console.log(id+" moved");
+        users[id].move(direction);
+    },
+    getUsers: function(){
+        return users;
+    },
+    height: height,
+    width: width
 };
--- index.js
+++ index.js
@@ -1,25 +1,33 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
-var game = require("./game")();
+var game = require("./game");
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-    game.addUser(socket.id);
-    socket.emit("setsize",{height: game.height,width: game.width});
+	let user = game.addUser(socket.id,(a,b)=>socket.emit(a,b));
+	socket.emit("setsize",{height: game.height,width: game.width});
     socket.on("moved",function(key){
-        game.move(socket.id,key.key);
-    });
+		if("game" in user)
+			game.move(socket.id,key.key);
+	});
+	socket.on("started",function(){
+		game.addUserToGame(user,(player1,player2)=>{
+			 setInterval(()=>{
+				player2.send("update",{players: [player1.toSendingData(),player2.toSendingData()]});
+			 	socket.emit("update",{players: [player1.toSendingData(),player2.toSendingData()]});
+			 },100);
+		});
+	});
+	
     socket.on('disconnect',function(){
         delete game.getUsers[socket.id];
     });
 });
-setInterval(()=>{
-    io.emit("update",{players: game.getUsers()});
-},100);
+
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
