7da021fe374aa3f5ddd7a125bec7379e0bf020fd
koldun256 <eg.sharypov@ya.ru>
2019-09-29 22:24:47 +0700

    d

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
-var game = require("./game");
+// var app = require('express')();
+// var http = require('http').createServer(app);
+// var io = require('socket.io')(http);
+// var game = require("./game");
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+// app.get('/', function(req, res){
+//   res.sendFile(__dirname + '/index.html');
+// });
 
-io.on('connection', function(socket){
-	let user = game.addUser(socket.id,(a,b)=>socket.emit(a,b));
-	socket.emit("setsize",{height: game.height,width: game.width});
-    socket.on("moved",function(key){
-		if("game" in user)
-			game.move(socket.id,key.key);
-	});
-	socket.on("started",function(){
-		game.addUserToGame(user,(player1,player2)=>{
-			 setInterval(()=>{
-				player2.send("update",{players: [player1.toSendingData(),player2.toSendingData()]});
-			 	socket.emit("update",{players: [player1.toSendingData(),player2.toSendingData()]});
-			 },100);
-		});
-	});
-	//a
-    socket.on('disconnect',function(){
-        delete game.getUsers[socket.id];
-    });
-});
+// io.on('connection', function(socket){
+// 	let user = game.addUser(socket.id,(a,b)=>socket.emit(a,b));
+// 	socket.emit("setsize",{height: game.height,width: game.width});
+//     socket.on("moved",function(key){
+// 		if("game" in user)
+// 			game.move(socket.id,key.key);
+// 	});
+// 	socket.on("started",function(){
+// 		game.addUserToGame(user,(player1,player2)=>{
+// 			 setInterval(()=>{
+// 				player2.send("update",{players: [player1.toSendingData(),player2.toSendingData()]});
+// 			 	socket.emit("update",{players: [player1.toSendingData(),player2.toSendingData()]});
+// 			 },100);
+// 		});
+// 	});
+// 	//a
+//     socket.on('disconnect',function(){
+//         delete game.getUsers[socket.id];
+//     });
+// });
 
-http.listen(21, function(){
-  console.log('listening on *:3000');
-});
+// http.listen(21, function(){
+//   console.log('listening on *:3000');
+// });
