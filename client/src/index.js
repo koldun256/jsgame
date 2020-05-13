da727e27fa8765c6bb740f7923c0b7c2f2bcef38
koldun256 <egor.sharypov2005@gmail.com>
2020-05-13 15:04:15 +0700

    added npm scripts;created client socket connection

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import io from 'socket.io-client'
+import config from './config.json'
+const socket = io(config['socket-path'])
 const Index = () => {
-  return <div>Hello React!</div>;
-};
+  return <div>Hello React!</div>
+}
 console.log('run js')
 ReactDOM.render(<Index />, document.getElementById("root"));
